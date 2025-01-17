df110fedf7eb1ed95704a7de11de3e31dd124b52
Devis Sandy <118913747+iamyss-t-me@users.noreply.github.com>
2025-01-17 16:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
-import os
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
+import os
+from datetime import datetime   
+import pytz
+import requests
+token = os.getenv('TELEGRAM_TOKEN')
+chat_id = os.getenv('TELEGRAM_CHAT_ID')
 
-TOKEN = '5481103194:AAG4QwvLnZ7k1_7NeLXxLh4226bFFHVqhiw'
-CHAT_IDS = [1276109349]
-def check():
-    url = 'https://ww2.5movierulz.mx/telugu-movie/'
-    req = requests.get(url).content
-    soup = BeautifulSoup(req,'html.parser')
-    divs = soup.find_all("div",class_="cont_display")
-    f = open("data.txt", "r").read()
-    title = divs[2].find("a")['title'].replace(" Watch Online Free",'')
-    image =  divs[2].find("img")['src']
-    link = divs[2].find("a")['href']
-    if title == f:
-        pass
-    else:
-        caption = "**New Movie**\n`{}`\n\n{}".format(title,link)
-        for CHAT_ID in CHAT_IDS:
-            send_message(CHAT_ID,caption,image)
-        os.remove("data.txt")
-        f = open("data.txt", "a")
-        f.write(title)
-        f.close()
+url = 'https://in.bookmyshow.com/explore/sports-vadodara'
 
+div_class = 'sc-dv5ht7-0 XmXCP'
+scraper = cloudscraper.create_scraper(
+    browser='chrome'
+)
+def check_wpl():
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    divs = soup.find_all('div',{'class':div_class})
+    for div in divs:
+        title = div.find('h3')
+        if "wpl" in title.text.lower():
+            print(title.text)
+            return True
+    return False
 
-def send_message(CHAT_ID,caption,img):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}&photo={img}&caption={caption}&parse_mode=MARKDOWN"
-    requests.post(url)
+def send_telegram_message(data):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    requests.post(url, data=data)
+
+def main():
+    try:
+        indian_time = datetime.now(pytz.timezone('Asia/Kolkata'))
+        data = {
+            'chat_id': chat_id,
+        }
+        if check_wpl():
+            data['text'] = f"WPL is available at {indian_time.strftime('%Y-%m-%d %H:%M:%S')}"
+            send_telegram_message(data)
+        else:
+            data['text'] = f"WPL is not available.\n\nChecked at {indian_time.strftime('%Y-%m-%d %H:%M:%S')}"
+            data['disable_notification'] = True
+            send_telegram_message(data)
+    except Exception as e:
+        data['text'] = f"Error: {e}\n\nChecked at {indian_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        send_telegram_message(data)
 
 if __name__ == "__main__":
-    check()
+    main()
